6e31616f484041c424ffebca20ca682f4bab2814
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-05-09 10:18:14 +0530

    registering error blueprints

--- blogwebsite/__init__.py
+++ blogwebsite/__init__.py
@@ -7,7 +7,10 @@ app=Flask(__name__)
 
 #registering core blueprint
 from blogwebsite.core.views import core
+#registering error_pages blueprint
+from blogwebsite.error_pages.handlers import error_pages
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
 
 
 
