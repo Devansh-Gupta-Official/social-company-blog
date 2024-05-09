79b4b6d92b4b35578baeafc28668a7ce1e329b8e
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-05-09 10:16:53 +0530

    working with 403 errors

--- blogwebsite/error_pages/handlers.py
+++ blogwebsite/error_pages/handlers.py
@@ -7,3 +7,7 @@ error_pages=Blueprint('error_pages',__name__)
 @error_pages.app_errorhandler(404)
 def error_404(error):
     return render_template('error_pages/404.html'),404  #404 is the status code 
+
+@error_pages.app_errorhandler(403)
+def error_403(error):
+    return render_template('error_pages/403.html'),403  #404 is the status code 
