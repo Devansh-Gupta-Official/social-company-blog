98aa068f12a896f549a5ca26ee44c13d5f271bde
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-05-09 10:25:20 +0530

    setting up the database

--- blogwebsite/__init__.py
+++ blogwebsite/__init__.py
@@ -1,9 +1,23 @@
 #holds organization of the application like connecting blueprints, and everything together, etc.
 #blogwebsite/__init__.py
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 app=Flask(__name__)
+
+#setting up the database
+basedir=os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+Migrate(app,db)
+#setup context **IMPORTANT**
+app.app_context().push()
+
+
 
 #registering core blueprint
 from blogwebsite.core.views import core
