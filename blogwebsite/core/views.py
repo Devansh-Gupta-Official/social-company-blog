ffb98e3dcba1f4180fd94a92ee4b66b4bf9123aa
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-05-09 10:05:35 +0530

    creating two core views

--- blogwebsite/core/views.py
+++ blogwebsite/core/views.py
@@ -1,1 +1,14 @@
 #home view and info view
+
+from flask import render_template, request, Blueprint
+
+core  = Blueprint('core', __name__)
+
+@core.route('/')
+def index():
+    #MORE TO COME
+    return render_template('index.html')
+
+@core.route('/info')
+def info():
+    return render_template('info.html')
