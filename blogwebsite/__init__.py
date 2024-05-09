1df0badc2fe93e820c3ded1cfed7cc2c76b8671d
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-05-09 09:24:04 +0530

    setting up initial app structure

---
+++ blogwebsite/__init__.py
@@ -0,0 +1,9 @@
+#holds organization of the application like connecting blueprints, and everything together, etc.
+#blogwebsite/__init__.py
+
+from flask import Flask
+
+app=Flask(__name__)
+
+
+
---
+++ blogwebsite/models.py
@@ -0,0 +1,1 @@
+#user and blog post models
