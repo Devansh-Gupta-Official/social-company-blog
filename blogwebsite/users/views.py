3695834d71295edd561e4e04cae3b656e8a7d2cb
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-05-10 16:38:50 +0530

    creating a view for login

--- blogwebsite/users/views.py
+++ blogwebsite/users/views.py
@@ -27,3 +27,19 @@ def register():
         return redirect(url_for('users.login'))
     
     return render_template('register.html', form=form)
+
+@users.route('/login', methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user.check_password(form.password.data) and user is not None: #check_password is a method in models.py
+            login_user(user)  #inbuilt function that logs in the user automatically
+            flash('Logged in successfully!')
+            next=request.args.get('next')  #if the user was trying to access a page that required login, it will redirect to that page after login
+            if next==None or not next[0]=='/':
+                next=url_for('core.index')
+            
+            return redirect(next)
+    
+    return render_template('login.html', form=form)
