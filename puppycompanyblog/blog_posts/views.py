3e22bd33c4b2fd78e53494a6cd17a5dfe5fc5656
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-05-13 18:15:47 +0530

    create view

--- puppycompanyblog/blog_posts/views.py
+++ puppycompanyblog/blog_posts/views.py
@@ -1,5 +1,5 @@
 #creating views
-from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask import render_template, url_for, flash, redirect, request, Blueprint, abort
 from flask_login import current_user, login_required
 from puppycompanyblog import db
 from puppycompanyblog.models import BlogPost
@@ -29,3 +29,28 @@ def create_post():
 def blog_post(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
     return render_template('blog_post.html', title=blog_post.title, date=blog_post.date, post=blog_post)
+
+
+#UPDATE
+@blog_posts.route('/<int:blog_post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+
+    if blog_post.author != current_user:
+        abort(403)  #permission denied
+
+    form = BlogPostForm()
+
+    if form.validate_on_submit():
+        blog_post.title = form.title.data
+        blog_post.text = form.text.data
+        db.session.commit()
+        flash('Blog Post Updated')
+        return redirect(url_for('blog_posts.blog_post', blog_post_id=blog_post.id))
+
+    elif request.method == 'GET':
+        form.title.data = blog_post.title
+        form.text.data = blog_post.text
+
+    return render_template('create_post.html', title='Updating', form=form)
